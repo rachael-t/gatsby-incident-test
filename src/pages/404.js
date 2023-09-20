e2e53715e3c93ecb96b6d08ada2c31bcfb69faff
Rachael Thomas <54180641+rachael-t@users.noreply.github.com>
2023-09-20 11:47:28 -0600

    Add additional text to 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,6 +7,7 @@ const NotFoundPage = () => (
   <Layout>
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>Adding some text as a test.</p>
   </Layout>
 )
 
